2a29b4224ecabd0a745e13852bce6a522f4075cd
Naveen Pandey <naveenpandeymzp2002@gmail.com>
2023-01-18 15:41:37 +0530

    Again commit

--- Sub.java
+++ Sub.java
@@ -2,6 +2,6 @@ public class Sub{
     public static void main(String ar[]){
         int a=35;
         int b=23;
-        System.out.println(a-b);
+        System.out.println("The sub is ="+(a-b));
     }
 }
